835f15b6bbf0d7d0e1235533096b9d04effb3bf9
Natalia Leonteva <morein.mm@gmail.com>
2023-12-10 13:18:30 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,136 @@
+fun main() {
+
+    ChatService.createChat(1, 2, "Hello, User2")
+    ChatService.createChat(1, 3, "Hello, User3")
+    ChatService.createChat(2, 3, "Hello in 2-3 chat")
+    ChatService.sendMessage(1, Message(authorId = 1, text = "How are you?"))
+    ChatService.sendMessage(1, Message(authorId = 2, text = "I'm fine. And you?"))
+    ChatService.sendMessage(1, Message(authorId = 2, text = "What are you planning to do today?"))
+    ChatService.sendMessage(1, Message(authorId = 1, text = "I want to go to the cinema."))
+    ChatService.sendMessage(1, Message(authorId = 2, text = "And play football"))
+
+}
+
+data class Chat (
+    val id: Int,
+    val user1Id: Int,
+    val user2Id: Int,
+    var nextMessageId: Int = 2,
+    var messages: MutableList<Message>
+) {
+    fun printChat() {
+        println("ЧАТ ID = " + this.id)
+        println("Пользователь 1 id = " + this.user1Id)
+        println("Пользователь 2 id = " + this.user2Id)
+        println("id следующего сообщения = " + this.nextMessageId)
+        println("Сообщения:")
+        for (message in this.messages) {
+            message.printMessage()
+        }
+        println("___________________")
+    }
+}
+
+data class Message (
+    val id: Int = 0,
+    val authorId: Int,
+    val text: String,
+    var read: Boolean = false
+) {
+    fun printMessage() {
+        println("ID = " + this.id)
+        println("Автор = " + this.authorId)
+        println("Текст = " + this.text)
+        println("Прочитано = " + this.read)
+    }
+}
+
+object ChatService {
+    private var chats = mutableListOf<Chat>()
+    private var nextChatId = 1
+
+
+    fun createChat(user1Id: Int, user2Id: Int, messageText: String) : Chat {
+        chats += Chat(nextChatId, user1Id, user2Id, messages  = mutableListOf(Message(1, user1Id, messageText)))
+        nextChatId ++
+        return chats.last()
+    }
+
+    fun printAllChats() {
+        for (chat in chats) {
+            chat.printChat()
+        }
+    }
+
+    fun deleteChat(chatId: Int) : Boolean {
+        if (!chats.removeIf { chat: Chat -> chat.id == chatId }) throw ChatNotFoundException("Chat not found by id")
+        return true
+    }
+
+    fun sendMessage(chatId: Int, message: Message) : Message {
+        val chat = getChatById(chatId)
+        chat.messages += message.copy(id = chat.nextMessageId)
+        chat.nextMessageId ++
+        return chat.messages.last()
+    }
+
+    fun getChatById(chatID: Int) : Chat {
+        val lst = chats.filter { chat: Chat -> chat.id == chatID }
+        if (lst.size != 1) throw ChatNotFoundException("Chat not found by id")
+        return lst[0]
+    }
+
+    fun deleteMessage(chatId: Int, messageId: Int) : Boolean {
+        val chat = getChatById(chatId)
+        if (!chat.messages.removeIf { message: Message -> message.id == messageId }) throw MessageNotFoundException("Message not found by id")
+        if (chat.messages.isEmpty()) {
+            deleteChat(chatId)
+        }
+        return true
+    }
+
+    fun getChats(userId:Int) : List<Chat> = chats.filter { chat: Chat -> chat.user1Id == userId } + chats.filter { chat: Chat -> chat.user2Id == userId }
+
+    fun getUnreadChatsCount(userId: Int) : Int {
+        //непрочитанное сообщение - если message.authorID != текущий userID и message.read = false
+        return getChats(userId).filter { chat: Chat -> return@filter getUnreadMessagesFromChat(chat, userId).count() > 0 }.count()
+    }
+
+    fun getNotMyMessagesFromChat(chat:Chat, userId: Int) = chat.messages.filter { message -> message.authorId != userId }
+
+    fun getUnreadMessagesFromChat(chat:Chat, userId: Int) = getNotMyMessagesFromChat(chat, userId).filter { message -> !message.read }
+
+
+    fun getChatsLastMessages(userId:Int) : List<String> {
+        var lastMessages = mutableListOf<String>()
+        for (chat in getChats(userId)) {
+            lastMessages += chat.messages.last().text
+        }
+        return lastMessages
+    }
+
+    fun getMessagesFromChat(currentUserId: Int, companionUserId: Int, numberOfMessages: Int) : List<Message>  {
+        var messagesList = listOf<Message>()
+        val chat = getChats(currentUserId).intersect(getChats(companionUserId).toSet())
+        if (chat.count() == 1) {
+            val toIndex = chat.elementAt(0).messages.size
+            var fromIndex = chat.elementAt(0).messages.size - numberOfMessages
+            if (fromIndex < 0) {fromIndex = 0}
+            messagesList = chat.elementAt(0).messages.subList(fromIndex, toIndex)
+            for (message in messagesList) {
+                if (message.authorId == companionUserId) {message.read = true}
+            }
+        }
+        return messagesList
+    }
+
+    fun clear() {
+        chats = mutableListOf<Chat>()
+        nextChatId = 1
+    }
+
+}
+
+class ChatNotFoundException(message: String) : RuntimeException(message)
+
+class MessageNotFoundException(message: String) : RuntimeException(message)
---
+++ src/test/kotlin/ChatServiceTest.kt
@@ -0,0 +1,102 @@
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class ChatServiceTest {
+
+    @Before
+    fun clearBeforeTest() {
+        ChatService.clear()
+    }
+
+    @Test
+    fun createChat() {
+        val chat = ChatService.createChat(1, 2, "Hello, User2")
+        assertEquals(1, chat.id)
+    }
+
+    @Test
+    fun deleteChatSuccess() {
+        val chat = ChatService.createChat(1, 2, "Hello, User2")
+        val result = ChatService.deleteChat(chat.id)
+        assertTrue(result)
+    }
+
+    @Test(expected = ChatNotFoundException::class)
+    fun deleteChatChatNotFoundException() {
+        ChatService.deleteChat(1)
+    }
+
+    @Test
+    fun sendMessageSuccess() {
+        val chat = ChatService.createChat(1, 2, "Hello, User2")
+        val message = ChatService.sendMessage(chat.id, Message(authorId = 1, text = "How are you?"))
+        assertEquals(2, message.id)
+    }
+
+    @Test(expected = ChatNotFoundException::class)
+    fun sendMessageChatNotFoundException() {
+        val message = ChatService.sendMessage(1, Message(authorId = 1, text = "How are you?"))
+    }
+
+    @Test
+    fun getChatByIdSuccess() {
+        val chat = ChatService.createChat(1, 2, "Hello, User2")
+        val getChat = ChatService.getChatById(chat.id)
+        assertEquals(chat.id, getChat.id)
+    }
+
+    @Test(expected = ChatNotFoundException::class)
+    fun getChatByIdChatNotFoundException() {
+        ChatService.getChatById(10)
+    }
+
+    @Test
+    fun deleteMessageSuccess() {
+        val chat = ChatService.createChat(1, 2, "Hello, User2")
+        ChatService.sendMessage(chat.id, Message(authorId = 1, text = "How are you?"))
+        val result = ChatService.deleteMessage(1, 2)
+        assertTrue(result)
+    }
+
+    @Test(expected = MessageNotFoundException::class)
+    fun deleteMessageMessageNotFoundException() {
+        val chat = ChatService.createChat(1, 2, "Hello, User2")
+        ChatService.deleteMessage(chat.id, 2)
+    }
+
+    @Test
+    fun getChats() {
+        var chat = ChatService.createChat(1, 2, "Hello, User2")
+        ChatService.createChat(3, 2, "Hello, User1")
+        var result = ChatService.getChats(1)
+        assertEquals(chat, result.get(0))
+    }
+
+    @Test
+    fun getUnreadChatsCount() {
+        ChatService.createChat(1, 2, "Hello, User2")
+        ChatService.createChat(2, 1, "Hello, User1")
+        var result = ChatService.getUnreadChatsCount(1)
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun getChatsLastMessages() {
+        val lstMessage = listOf("Hello, User2", "Hello, User1")
+        ChatService.createChat(1, 2, lstMessage.get(0))
+        ChatService.createChat(2, 1, lstMessage.get(1))
+        val lstResult = ChatService.getChatsLastMessages(1)
+        assertEquals(lstMessage, lstResult)
+    }
+
+    @Test
+    fun getMessagesFromChat() {
+        val messageText = "Hello, User2"
+        ChatService.createChat(1, 2, messageText)
+        val lstResult = ChatService.getMessagesFromChat(1, 2, 1)
+        assertEquals(messageText, lstResult.get(0).text)
+
+    }
+}
